8962e35fcf5eb1b82fb98dce0a614d361eb0a5dc
Erwan Guyader <erwan.guyader@cozycloud.cc>
2019-07-31 16:01:45 +0200

    Update setup script

--- setup.sh
+++ setup.sh
@@ -35,26 +35,54 @@ git submodule init
 git submodule update
 
 # Install Vimproc plugin
-echo "-- Installing Vimproc"
-cd vim/bundle/vimproc.vim/ && make
+#echo "-- Installing Vimproc"
+#cd vim/bundle/vimproc.vim/ && make
 
 # Check for presence of xsel to access system clipboard
 echo "-- Checking for xsel presence"
 # [ command -v xsel ]
+sudo dnf install xsel
 
 cd $CURRENTDIR
 echo "[2] Done."
 ##     ##
 
 # Install LAFAYETTE layout
-echo "[3] Setting up keyboard layout..."
-echo "export LAYOUT_PATH=$HOME/dotfiles/lafayette.xkb" >> $HOME/.bashrc
-echo "$HOME/dotfiles/set-keyboard-layout.sh" >> $HOME/.bashrc
-echo "[3] Done."
+#echo "[3] Setting up keyboard layout..."
+#echo "export LAYOUT_PATH=$HOME/dotfiles/lafayette.xkb" >> $HOME/.bashrc
+#echo "$HOME/dotfiles/set-keyboard-layout.sh" >> $HOME/.bashrc
+#echo "[3] Done."
+
+# Install Alacritty
+echo "[4] Installing Alacritty..."
+echo "-- Installing latest version of rust"
+curl https://sh.rustup.rs -sSf | sh
+rustup override set stable
+rustup update stable
+
+echo "-- Installing build dependencies"
+sudo dnf install cmake freetype-devel fontconfig-devel xclip
+
+echo "-- Downloading Alacritty"
+cargo install --git https://github.com/jwilm/alacritty
+
+echo "-- Adding desktop entry"
+wget https://raw.githubusercontent.com/jwilm/alacritty/master/alacritty.desktop
+desktop-file-install alacritty.desktop --dir=/home/erwan/.local/share/applications
+rm alacritty.desktop
+
+echo "-- Installing bash completions"
+mkdir -p ~/.bash_completion
+cd ~/.bash_completion
+wget https://raw.githubusercontent.com/jwilm/alacritty/master/alacritty-completions.bash -O alacritty
+echo "source ~/.bash_completion/alacritty" >> ~/.bashrc
+
+echo "[4] Done."
+# ##
 
 # Load user config
-echo "[4] Loading your configuration..."
+echo "[5] Loading your configuration..."
 . $HOME/.bashrc
-echo "[4] Done."
+echo "[5] Done."
 
 echo "\nSetup done!"
